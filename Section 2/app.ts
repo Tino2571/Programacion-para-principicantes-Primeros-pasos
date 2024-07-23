ebb77818718f08fa1eec56cc5d9b0e3296de52c0
Tino2571 <faustinofierro2571@gmail.com>
2024-07-22 17:25:33 -0700

    Update app.ts

--- Section 2/app.ts
+++ Section 2/app.ts
@@ -1,11 +1,10 @@
-let presentLine = 0;
-
 function printLineNumbers(numberOfLines: any) {
+  let presentLine = 0;
+
   for (let i = 0; i < numberOfLines; i++) {
     presentLine++;
     console.log("Línea #", presentLine);
   }
-  presentLine = 0;
 }
 
 printLineNumbers(180);
